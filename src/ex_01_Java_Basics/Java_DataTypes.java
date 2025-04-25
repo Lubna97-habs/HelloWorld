18e9492bab8f94dc5fe40399f826ff686c8d7b47
Lubna <lubnaanjum7863@gmail.com>
2025-04-25 08:48:02 +0530

    dataTypes

---
+++ src/ex_01_Java_Basics/Java_DataTypes.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basics;
+
+public class Java_DataTypes {
+    public static void main(String[] args) {
+        int a=10;
+        System.out.printf("%d",a);
+
+        int b=20;
+        System.out.println("-------------");
+        System.out.printf("%d + %d", a, b);
+    }
+}
