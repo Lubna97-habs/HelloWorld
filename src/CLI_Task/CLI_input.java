06d2551044bcf40b0b7435a8f545375c126aaa08
Lubna <lubnaanjum7863@gmail.com>
2025-04-25 15:30:39 +0530

    CLI_task_printInputs

---
+++ src/CLI_Task/CLI_input.java
@@ -0,0 +1,12 @@
+package CLI_Task;
+
+public class CLI_input {
+    public static void main(String[] args) {
+        String name = args[0];
+        String  age = args[1];
+        String salary = args[2];
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(salary);
+    }
+}
