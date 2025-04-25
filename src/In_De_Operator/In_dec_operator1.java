e4179b97466d357e0be50b9e01975e16db31130c
Lubna <lubnaanjum7863@gmail.com>
2025-04-25 15:22:15 +0530

    Increment_Decrement_opt2

---
+++ src/In_De_Operator/In_dec_operator1.java
@@ -0,0 +1,9 @@
+package In_De_Operator;
+
+public class In_dec_operator1 {
+    public static void main(String[] args) {
+        int a=20;
+        System.out.println(--a + a++ + a--);
+        System.out.println(a);
+    }
+}
